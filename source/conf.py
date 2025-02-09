27d7c99a6eaa7d62a7762c44766ac3f85603052f
JohnGAB7 <johngabary@hotmail.com>
2025-02-09 18:45:06 +0100

    Mise à Jour du Site 09/02/2025

--- source/conf.py
+++ source/conf.py
@@ -45,11 +45,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- HTML output options -----------------------------------------------------
 html_theme = "pydata_sphinx_theme"
 html_logo = "_static/photo.png"
-html_favicon = "_static/log.png"
+html_favicon = "_static/photo.png"
 html_sourcelink_suffix = ""
 html_last_updated_fmt = ""
 # html_extra_path = ['_static']
-html_title = "JG's World"
+html_title = "John_GABARY"
 
 # PyData theme customization
 html_theme_options = {
