2cc3d364859fa781ae9250b39f604a8ef83c2351
JohnGAB7 <johngabary@hotmail.com>
2025-06-15 18:53:08 +0200

    Déploiement du 16/06/2025

--- source/conf.py
+++ source/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 project = "JG World"
 author = 'John GABARY'
-copyright = '2024, John GABARY'
+copyright = '2025'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
